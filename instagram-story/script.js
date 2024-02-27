6b1a2d860ce76448fd48ca9ea9ccea51b3c4a33e
Junaid Khan <junaid.khan45694@gmail.com>
2024-02-28 00:36:14 +0500

    .

--- instagram-story/script.js
+++ instagram-story/script.js
@@ -1,3 +1,4 @@
+// Junaid Khan Frontend Developer
 var arr = [
     { dp: "https://images.unsplash.com/photo-1708607728910-b00e0c1be0af?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", story: "https://images.unsplash.com/photo-1708896149006-40a4755c42f2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
 
@@ -39,3 +40,4 @@ storyFrame.addEventListener('click', function (dets) {
     }, 3000);
 
 });
+// Junaid Khan Frontend Developer
