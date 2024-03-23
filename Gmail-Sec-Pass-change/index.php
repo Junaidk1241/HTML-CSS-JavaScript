7f73d0486fe6f8b496bff1af254be892f8d13aea
Junaid Khan <147981344+Junaidk1241@users.noreply.github.com>
2024-03-23 11:10:55 -0700

    .

--- Gmail-Sec-Pass-change/index.php
+++ Gmail-Sec-Pass-change/index.php
@@ -19,7 +19,7 @@
 
         </div>
         <h2>Change password</h2>
-        <div id="gmail"><i class="fa-regular fa-user"></i> musaakhann031@gmail.com 
+        <div id="gmail"> <i class="fa-regular fa-user"></i>  musaakhann031@gmail.com 
         <i class="fa-solid fa-caret-down"></i>
             </div>
 
