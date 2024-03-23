41a72d2c928ba0950c840e0bd08ad4ba44670736
Junaid Khan <147981344+Junaidk1241@users.noreply.github.com>
2024-03-23 11:05:45 -0700

    .

--- Gmail-Sec-Pass-change/index.php
+++ Gmail-Sec-Pass-change/index.php
@@ -41,28 +41,7 @@
         </form>
     </div>
     <script>
-        const togglePassword = document.querySelector('#togglePassword');
-        const toggleePassword = document.querySelector('#togglePassword');
-        const password = document.querySelector('#cur-pass');
-        const Newpassword = document.querySelector('#new-pass');
-
-
-
-        togglePassword.addEventListener('click', function (e) {
-            // toggle the type attribute
-            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-            password.setAttribute('type', type);
-            // toggle the eye slash icon
-            this.classList.toggle('fa-eye-slash');
-        });
-
-        toggleePassword.addEventListener('click', function (e) {
-            // toggle the type attribute
-            const type = Newpassword.getAttribute('type') === 'password' ? 'text' : 'password';
-            Newpassword.setAttribute('type', type);
-            // toggle the eye slash icon
-            this.classList.toggle('fa-eye-slash');
-        });
+       
     </script>
 
 
